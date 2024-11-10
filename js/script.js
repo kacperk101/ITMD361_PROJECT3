742d202251faec1ca09ee1297f50e6cf7644eae3
Kacper K <kacper3525@yahoo.com>
2024-11-09 20:53:04 -0600

    Adding a more simple box slider to my website
    The last box slider was very complex so I opted for a simpler one. I researched js code for a simple box slider

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   let currentIndex = 0;
-  const images = document.querySelectorAll("#wowslider-container1 .ws_images li");
-  const bullets = document.querySelectorAll(".ws_bullets a");
+  const images = document.querySelectorAll(".slider");
 
-  const updateSlider = () => {
-    images.forEach((img, i) => img.style.display = i === currentIndex ? "block" : "none");
-    bullets.forEach((bullet, i) => bullet.classList.toggle("active", i === currentIndex));
+  const showImage = () => {
+    images.forEach((img, index) => {
+      img.style.display = index === currentIndex ? "block" : "none";
+    });
   };
-
-  bullets.forEach((bullet, i) => bullet.addEventListener("click", () => {
-    currentIndex = i;
-    updateSlider();
-  }));
 
   setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
-    updateSlider();
+    showImage();
   }, 3000);
 
-  updateSlider();
+  showImage();
 });
